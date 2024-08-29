0023f2910d7bfd66cb9b979e743c77a398a6cd0b
bbel04 <158131166+bbel04@users.noreply.github.com>
2024-08-29 13:35:01 -0500

    Factorizer.java implementation, added unit tests
    Implement PrimeFactor method in Factorizer.java to return the prime factors of an int given a number. Added unit tests to TestFactorizer.java

--- src/main/java/com/csc/Factorizer.java
+++ src/main/java/com/csc/Factorizer.java
@@ -1,14 +1,32 @@
 package com.csc;
 
 public class Factorizer {
+  public String PrimeFactor(int number) {
 
-  // You should implement your solution here.
-  // Feel free to delete this example method when you implement your solution.
-  public int exampleMethod(int someArgument) {
-    if (someArgument > 5) {
-      return 1;
-    } else {
-      return 2;
+    int originalNumber = number;
+
+    if (number <= 1) {   //if number is less than or equal to 1, it does not have any prime factors
+      return number + " has no prime factors";
+    }
+
+    String primeFactors = "";   //initialize empty string to store prime factors
+
+    for (int i = 2; i <= Math.sqrt(number); i++) {   //iterate through the given number starting at two up until its square root
+      while (number % i == 0) {    
+        primeFactors += i + " ";      //if the number is divisible by the current iteration, it is a prime factor -- append to string
+        number /= i;                  // divide number by current iteration, repeat until number cannot be equally divided by the iteration
+      }      
+    }
+
+    if (number > 2) {   //If number is still greater than two, it is a prime number
+      primeFactors += number;   
+    }
+
+    if (primeFactors.trim().equals(Integer.toString(originalNumber))) {  //if primeFactors is still equal to original number, it must be prime
+      return originalNumber + " 1";      //if yes, return the original number and 1 as factors
+    }
+    else {   
+      return primeFactors.trim();  
     }
   }
 }
--- src/test/java/com/csc/TestFactorizer.java
+++ src/test/java/com/csc/TestFactorizer.java
@@ -1,5 +1,6 @@
 package com.csc;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -16,18 +17,58 @@ public class TestFactorizer {
 
   // Replace the following example tests with your own tests
   // of the prime factorization function.
+   
   @Test
-  public void testZero() {
-    assertEquals(2, factorizer.exampleMethod(0));
+  public void testPrimeFactorsOfZero() {
+    String expected = "0 has no prime factors";
+    assertEquals(expected, factorizer.PrimeFactor(0));
   }
 
   @Test
-  public void testOne() {
-    assertEquals(2, factorizer.exampleMethod(1));
+  public void testPrimeFactorsOfOne() {
+    String expected = "1 has no prime factors";
+    assertEquals(expected, factorizer.PrimeFactor(1));
+  }
+  
+  @Test
+  public void testPrimeFactorsOfThree() {
+    String expected = "3 1";
+    assertEquals(expected, factorizer.PrimeFactor(3));
   }
 
   @Test
-  public void testTen() {
-    assertEquals(1, factorizer.exampleMethod(10));
+  public void testPrimeFactorsOfTen() {
+    String expected = "2 5";
+    assertEquals(expected, factorizer.PrimeFactor(10));
+  }
+
+  @Test
+    public void testPrimeFactorsOfTwelve() {
+      String expected = "2 2 3";
+      assertEquals(expected, factorizer.PrimeFactor(12));
+    }
+
+  @Test
+    public void testPrimeFactorsOfThriteen() {
+      String expected = "13 1";
+      assertEquals(expected, factorizer.PrimeFactor(13));
+    }
+
+  @Test
+    public void testPrimeFactorsOfTwentyFour() {
+      String expected = "2 2 2 3";
+      assertEquals(expected, factorizer.PrimeFactor(24));
+    }
+
+  @Test
+  public void testPrimeFactorsOfFourtyOne() {
+    String expected = "41 1";
+    assertEquals(expected, factorizer.PrimeFactor(41));
+  }
+
+  @Test
+  public void testPrimeFactorsOfOneHundred() {
+    String expected = "2 2 5 5";
+    assertEquals(expected, factorizer.PrimeFactor(100));
   }
 }
